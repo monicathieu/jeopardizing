53effc88e235edd68a88893971fe1a280bedf49e
monicathieu <mkt2131@columbia.edu>
2021-01-08 11:44:08 -0500

    revise retrieval_pics code

--- R/get_master_spreadsheet.R
+++ R/get_master_spreadsheet.R
@@ -89,21 +89,19 @@ encoding <- master_spreadsheet %>%
 
 retrieval_facts <- master_spreadsheet
 
-master_spreadsheet$randomise_trials <- seq.int(nrow(master_spreadsheet))
-
 retrieval_pics <- master_spreadsheet %>%
-  select(randomise_trials,
-          randomise_blocks,
-          trial_num,
-          group,
-  starts_with("randomise_trials"),
-       starts_with("pic_c"),
-       starts_with("pic_d")) %>% 
-  arrange(randomise_trials) %>% 
-  mutate(display = "memorise") %>% 
-  bind_rows(tibble(display = "study_instructions"),
+  select(trial_num,
+         group,
+         starts_with("pic_c"),
+         starts_with("pic_d")) %>% 
+  mutate(randomise_trials = 1L,
+         display = "mem_intrusion") %>% 
+  select(trial_num,
+         display,
+         randomise_trials,
+         everything()) %>% 
+  bind_rows(tibble(display = "mem_intrusion_instructions"),
             .,
             tibble(display = "finish"))
-#hacks, need to create a randomize trials column
 
 # TODO: Save widened spreadsheet to csv
