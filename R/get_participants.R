8f1f64c0b19d6e12f92cfcf852f2ad891ff79770
monicathieu <mkt2131@columbia.edu>
2021-07-12 15:03:33 -0400

    add checking for rundle d and e

--- R/get_participants.R
+++ R/get_participants.R
@@ -13,11 +13,14 @@ inquiries <- inquiries_raw %>%
   filter(consent == "YES, I would like to continue.",
          interested == "Yes",
          !is.na(email)) %>% 
-  select(public_id = ResponseId, email) %>% 
+  select(public_id = ResponseId, email, inquiry_date = EndDate) %>% 
   mutate(name = public_id,
          group = "")
 
-write_csv(inquiries, here::here("ignore", "running_subjs", "participants_gorilla_upload.csv"))
+# just people after the LL posts were amended to say low rundle only 
+inquiries %>% 
+  filter(inquiry_date >= "2021-07-06 21:30:00") %>% 
+  write_csv(here::here("ignore", "running_subjs", "participants_gorilla_upload.csv"))
 
 ## read in completion status from gorilla manual download ----
 
@@ -57,7 +60,39 @@ payments <- completions %>%
 
 # Write out only one pay file per pay batch (with pay date as identifier)
 
-write_csv(payments, here::here("ignore",
-                               "running_subjs",
-                               glue::glue("participants_tango_upload_{str_remove_all(Sys.Date(), '-')}.csv")),
+write_csv(payments,
+          here::here("ignore",
+                     "running_subjs",
+                     glue::glue("participants_tango_upload_{str_remove_all(Sys.Date(), '-')}.csv")),
           na = "")
+
+## write out emails for rundle certification secondary prescreen ----
+
+# run at noon-ish Pacific time July 6 2021 
+inquiries_norundle <- inquiries %>% 
+  left_join(completions %>% 
+              select(email, status),
+            by = "email") %>% 
+  # People who have already started/finished the study don't need to certify
+  filter(!(status %in% c("Live", "Complete"))) %>% 
+  select(email)
+
+write_csv(inquiries_norundle,
+          here::here("ignore",
+                     "running_subjs",
+                     "participants_rundle_prescreen_upload_20210706.csv"))
+
+## get data from rundle secondary prescreen ----
+
+prescreen_rundle_raw <- fetch_survey("SV_4101dXMbVZpTADc",
+                                     force_request = TRUE,
+                                     label = TRUE)
+
+prescreen_rundle <- prescreen_rundle_raw %>% 
+  select(email = RecipientEmail, rundle, interested)
+
+prescreen_rundle %>% 
+  filter(interested == "Yes") %>% 
+  select(email) %>% 
+  left_join(inquiries, by = "email") %>% 
+write_csv(here::here("ignore", "running_subjs", "participants_gorilla_upload_lowrundles.csv"))
