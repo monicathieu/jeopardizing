23795b353a0e07c6c43e289e39725dce00e1deab
monicathieu <mkt2131@columbia.edu>
2021-10-18 10:48:33 -0400

    move general cleaned data loading to own script

--- R/get_cleaned_data.R
+++ R/get_cleaned_data.R
@@ -24,13 +24,16 @@ j_meta <- read_csv(here::here("ignore", "data", "task_jeopardy_meta_states.csv")
 j_meta_desc <- read_csv(here::here("ignore", "data", "task_jeopardy_meta_descriptions.csv")) %>% 
   left_join(j_exp, by = "subj_num") %>% 
   mutate(subj_num = fct_reorder(as.character(subj_num), j_score))
+
 encoding <- read_csv(here::here("ignore", "data", "task_encoding.csv")) %>% 
   left_join(j_exp, by = "subj_num") %>% 
   mutate(subj_num = fct_reorder(as.character(subj_num), j_score),
          already_knew = fct_recode(already_knew,
                                    none = "Did not know this before",
                                    some = "Knew some of this before",
-                                   all = "Knew all of this before"))
+                                   all = "Knew all of this before"),
+         already_knew = fct_relevel(already_knew, "all", after = Inf))
+
 r_facts <- read_csv(here::here("ignore", "data",  "task_retrieval_facts.csv")) %>% 
   left_join(j_exp, by = "subj_num") %>% 
   mutate(subj_num = fct_reorder(as.character(subj_num), j_score))
@@ -54,11 +57,16 @@ retrieval <- r_facts %>%
               select(subj_num, group, group_trial_num, category, resp_source = resp, rt_source = rt)) %>%
   left_join(encoding %>% 
               select(subj_num, group, group_trial_num, category, interest = resp, already_knew)) %>% 
-  mutate(retrieval_block_type = case_when(retrieval_block <= 4 ~ "ep_same_sem_same",
-                                          retrieval_block <= 8 ~ "ep_same_sem_diff",
-                                          retrieval_block <= 12 ~ "ep_diff_sem_same",
-                                          retrieval_block <= 16 ~ "ep_diff_sem_diff",
+  mutate(retrieval_block_type = case_when(ep_is_same == 1 & sem_is_same == 1 ~ "ep_same_sem_same",
+                                          ep_is_same == 1 & sem_is_same == 0 ~ "ep_same_sem_diff",
+                                          ep_is_same == 0 & sem_is_same == 1 ~ "ep_diff_sem_same",
+                                          ep_is_same == 0 & sem_is_same == 0 ~ "ep_diff_sem_diff",
                                           TRUE ~ NA_character_),
+  #mutate(retrieval_block_type = case_when(retrieval_block <= 4 ~ "ep_same_sem_same",
+   #                                       retrieval_block <= 8 ~ "ep_same_sem_diff",
+    #                                      retrieval_block <= 12 ~ "ep_diff_sem_same",
+     #                                     retrieval_block <= 16 ~ "ep_diff_sem_diff",
+      #                                    TRUE ~ NA_character_),
          encoding_trial_num = if_else(group == "academic",
                                       group_trial_num * 2 - 1,
                                       group_trial_num * 2))
